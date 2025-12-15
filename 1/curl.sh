9b29d9315fc64694ee837eb57dfadefc09304629
a.nosov@dreamsoft.by <a.nosov@dreamsoft.by>
2025-12-15 12:38:39 +0300

    1

---
+++ 1/curl.sh
@@ -0,0 +1,9 @@
+curl -X POST http://localhost:5678/webhook-test/fc82ef08-f760-4bd6-8bab-29175e8e16a5 \
+
+curl -X POST http://localhost:5678/webhook/fc82ef08-f760-4bd6-8bab-29175e8e16a5 \
+ -H "Content-Type: application/json" \
+ -d '{
+    "chatInput": "Привет, как дела?",
+    "sessionId": "test-session-1",
+    "prompt":"answer simple"
+}'
