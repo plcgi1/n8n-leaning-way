ed13d6dd2a3afbb12acaad6edd83d4bea917b4b7
a.nosov@dreamsoft.by <a.nosov@dreamsoft.by>
2025-12-15 12:39:08 +0300

    1 #1

--- 1/curl.sh
+++ 1/curl.sh
@@ -1,3 +1,4 @@
+# для тестов
 curl -X POST http://localhost:5678/webhook-test/fc82ef08-f760-4bd6-8bab-29175e8e16a5 \
 
 curl -X POST http://localhost:5678/webhook/fc82ef08-f760-4bd6-8bab-29175e8e16a5 \
